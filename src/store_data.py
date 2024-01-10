3b99e000d5b7b60db2316339cbde319bc3f31dea
AlexanderVVasilenko <alexander_vasilenko@outlook.com>
2024-01-10 21:30:00 +0200

    fix second trying to store data

--- src/store_data.py
+++ src/store_data.py
@@ -5,10 +5,16 @@ from package_interaction import get_driver_list
 
 def read_abbreviations_and_create_racers(file_path: str) -> None:
     driver_list = get_driver_list("asc", abbrs=load_abbreviations(file_path))
-    Racer.bulk_create([Racer(**driver) for driver in driver_list])
+
+    for driver in driver_list:
+        driver_id = driver["driver_id"]
+        existing_racer = Racer.select().where(Racer.driver_id == driver_id).first()
+
+        if not existing_racer:
+            Racer.create(**driver)
 
 
-def read_and_create_race(file_path: str) -> dict:
+def read_and_create_race(file_path: str) -> Race:
     with open(file_path, "r") as race_data:
         lines = race_data.readlines()
         year = lines[2].split(": ")[1].strip("\n")
@@ -25,25 +31,23 @@ def read_lap_time_and_create_races(
     racers = parse_logs(start_log_path, end_log_path, load_abbreviations(abbrs_path))
     racers = racers[0] + racers[1]
 
-    LapTime.bulk_create(
-        [
-            LapTime(
-                race=race,
-                racer=Racer.get(Racer.driver_id == racer.driver_id),
-                lap_time=racer.lap_time,
-            )
-            for racer in racers
-        ]
-    )
+    for racer in racers:
+        db_racer = Racer.get(Racer.driver_id == racer.driver_id)
+        existing_lap_time = (
+            LapTime.select()
+            .where((LapTime.race == race) & (LapTime.racer == db_racer))
+            .first()
+        )
+
+        if not existing_lap_time:
+            LapTime.create(race=race, racer=db_racer, lap_time=racer.lap_time)
 
 
 if __name__ == "__main__":
     db.connect()
 
     read_abbreviations_and_create_racers("inputs/abbreviations.txt")
-
     race_obj = read_and_create_race("inputs/race_data.txt")
-
     read_lap_time_and_create_races(
         "inputs/start.log", "inputs/end.log", "inputs/abbreviations.txt", race_obj
     )
