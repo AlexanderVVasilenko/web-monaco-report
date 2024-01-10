b1b5d1a5045924bf1db77d4c2eaf0e390a70790b
AlexanderVVasilenko <alexander_vasilenko@outlook.com>
2024-01-10 17:29:44 +0200

    refactoring

--- src/store_data.py
+++ src/store_data.py
@@ -5,8 +5,7 @@ from package_interaction import get_driver_list
 
 def read_abbreviations_and_create_racers(file_path: str) -> None:
     driver_list = get_driver_list("asc", abbrs=load_abbreviations(file_path))
-    for driver in driver_list:
-        Racer.create(**driver)
+    Racer.bulk_create([Racer(**driver) for driver in driver_list])
 
 
 def read_and_create_race(file_path: str) -> dict:
@@ -15,17 +14,29 @@ def read_and_create_race(file_path: str) -> dict:
         year = lines[2].split(": ")[1].strip("\n")
         location = lines[1].split(": ")[1].strip("\n")
         name = lines[0].split(": ")[1].strip("\n")
-    return Race.create(year=year, location=location, race_name=name)
+
+    race, _ = Race.get_or_create(year=year, location=location, race_name=name)
+    return race
 
 
-def read_lap_time_and_create_races(start_log_path: str, end_log_path: str, abbrs_path: str, race: Race) -> None:
+def read_lap_time_and_create_races(
+    start_log_path: str, end_log_path: str, abbrs_path: str, race: Race
+) -> None:
     racers = parse_logs(start_log_path, end_log_path, load_abbreviations(abbrs_path))
     racers = racers[0] + racers[1]
-    for racer in racers:
-        LapTime.create(race=race, racer=Racer.get(Racer.driver_id == racer.driver_id), lap_time=racer.lap_time)
+
+    LapTime.bulk_create(
+        [
+            LapTime(
+                race=race,
+                racer=Racer.get(Racer.driver_id == racer.driver_id),
+                lap_time=racer.lap_time,
+            )
+            for racer in racers
+        ]
+    )
 
 
-# Main script
 if __name__ == "__main__":
     db.connect()
 
@@ -33,6 +44,8 @@ if __name__ == "__main__":
 
     race_obj = read_and_create_race("inputs/race_data.txt")
 
-    read_lap_time_and_create_races("inputs/start.log", "inputs/end.log", "inputs/abbreviations.txt", race_obj)
+    read_lap_time_and_create_races(
+        "inputs/start.log", "inputs/end.log", "inputs/abbreviations.txt", race_obj
+    )
 
     db.close()
