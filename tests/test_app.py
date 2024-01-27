3633435d8fae07bfb8c2e2e06ffb4fa125e47e65
Oleksandr <alexander_vasilenko@outlook.com>
2024-01-27 04:08:58 +0200

    test_api.py: created with requests against flask.client

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,8 +1,4 @@
-# tests/test_app.py
-
-
 import pytest
-from flask.testing import FlaskClient
 from peewee import SqliteDatabase
 
 from api import api_app
@@ -13,10 +9,8 @@ from store_data import (
 )
 from models import Race, Racer, LapTime
 
-# Configure a test database
 db = SqliteDatabase(":memory:")
 
-# Override the database used by the models to the test database
 Race._meta.database = db
 Racer._meta.database = db
 LapTime._meta.database = db
@@ -54,33 +48,3 @@ def test_read_lap_time_and_create_races(client):
     )
 
     assert LapTime.select().count() == 20  # Assuming there are 20 racers in your logs
-
-
-def test_api_endpoints(client: FlaskClient):
-    read_abbreviations_and_create_racers("inputs/abbreviations.txt")
-    race_obj = read_and_create_race("inputs/race_data.txt")
-    read_lap_time_and_create_races(
-        "inputs/start.log", "inputs/end.log", "inputs/abbreviations.txt", race_obj
-    )
-
-    response = client.get("/api/v1/report")
-    assert response.status_code == 200
-    assert response.headers["Content-Type"] == "application/json"
-    assert "racers" in response.json
-
-    response = client.get("/api/v1/report/drivers")
-    assert response.status_code == 200
-    assert response.headers["Content-Type"] == "application/json"
-    assert "racers" in response.json
-
-    existing_driver_id = "RAI"  # Replace with an existing driver_id
-    response = client.get(f"/api/v1/report/drivers/{existing_driver_id}")
-    assert response.status_code == 200
-    assert response.headers["Content-Type"] == "application/json"
-    assert "racer" in response.json
-
-    non_existing_driver_id = "R"  # Replace with a non-existing driver_id
-    response = client.get(f"/api/v1/report/drivers/{non_existing_driver_id}")
-    assert response.status_code == 404
-    assert response.headers["Content-Type"] == "application/json"
-    assert "error" in response.json
